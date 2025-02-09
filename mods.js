d6670316436f3e472455bf2d2d3ad2170d823c67
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2025-02-09 02:53:05 -0800

    Create mods.js

---
+++ mods.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.id = 'MutationCounter';
+script.src = 'https://stellar-demesne.github.io/Trimps-MutationCounter/MutationCounter.js';
+script.setAttribute('crossorigin', "anonymous");
+document.head.appendChild(script);
